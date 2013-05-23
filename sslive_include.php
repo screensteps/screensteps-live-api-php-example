472dc80ffdcd1f6b69d8111b952a4886a6752ca4
Trevor DeVore <trevor@screensteps.com>
2013-05-23 00:28:45 -0400

    Removed mention of API Access key and changed recommendation to API user.

--- sslive_include.php
+++ sslive_include.php
@@ -2,11 +2,7 @@
 	// Include ScreenSteps Live class file
 	require_once('sslive_class.php');
 	
-	// Create ScreenSteps Live object using your domain and API key
-	//$sslive = new SSLiveAPI('example.screenstepslive.com', 'http');
-	//$sslive->SetAPIKey('12e5317e88');
-	
-	// Create ScreenSteps Live object using reader account credentials
+	// Create ScreenSteps Live object using API account credentials
 	$sslive = new SSLiveAPI('example.screenstepslive.com', 'http');
 	$sslive->SetUserCredentials('example', 'example');
 	
